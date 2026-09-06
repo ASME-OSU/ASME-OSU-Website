4b55fb8f4ff9cfe8b2b98fdf7b3d5442f4d59c25
Matthew Allen <matthewallen@Matthews-MacBook-Air-10.local>
2026-09-05 23:36:30 -0400

    Tighten desktop navigation and keep wide title on one line

--- scripts/build-header.mjs
+++ scripts/build-header.mjs
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 const html=fs.readFileSync('Header.html','utf8');
-const assets='<link id="asme-header-styles" rel="stylesheet" href="https://asme-osu.github.io/ASME-OSU-Website/Header.css?v=20260905-header7">\n<script src="https://asme-osu.github.io/ASME-OSU-Website/Header%20Integration.js?v=20260905-header7" defer></script>\n';
+const assets='<link id="asme-header-styles" rel="stylesheet" href="https://asme-osu.github.io/ASME-OSU-Website/Header.css?v=20260905-header8">\n<script src="https://asme-osu.github.io/ASME-OSU-Website/Header%20Integration.js?v=20260905-header8" defer></script>\n';
 fs.writeFileSync('Header Embed.html',html+'\n'+assets);
 const footer=fs.readFileSync('Footer.html','utf8').replace(/\n<!-- ASME HEADER COMPONENT START -->[\s\S]*?<!-- ASME HEADER COMPONENT END -->\n?/g,'');
 fs.writeFileSync('Footer.html',footer+'\n<!-- ASME HEADER COMPONENT START -->\n'+html+'\n'+assets+'<!-- ASME HEADER COMPONENT END -->\n');
